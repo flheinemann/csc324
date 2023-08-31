4d1c763006f65c0b82a706f8b8c53bebf0334295
Finn <flheinemann@georgetowncollege.edu>
2023-08-31 18:59:13 -0400

    Final Homework 2

--- ch02_Finn_Heinemann.js
+++ ch02_Finn_Heinemann.js
@@ -9,6 +9,40 @@ while(line.length<=7){
 }
 
 // FizzBuzz
+let current=1;
+
+while(current<=100){
+    if(current%3==0 & current%5==0){
+        console.log("FizzBuzz")
+    }
+    else if(current%3==0){
+        console.log("Fizz")
+    }
+    else if(current%5==0){
+        console.log("Buzz")
+    }
+    else{
+        console.log(current)
+    }
+    current =current+1
+}
 
 
 // ChessBoard
+
+let number = 7;
+let result= "";
+
+for(let column=0; column<number; column++){
+    for(let row=0; row<number; row++){
+        if((row+column) %2 ==0){
+            result=result+" ";
+        }
+        else{
+            result=result+"#";
+        }
+    }
+    result=result+"\n";
+}
+console.log(result);
+
